ebe35ccf0f85256234b3c87e3c48bba10be4f271
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-12 01:32:30 +0900

    Create BOJ 11654.cpp

---
+++ BOJ/BOJ 11654.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// 백준 11654
+
+int main(void)
+{
+    char a;
+    while (1){
+        cin >> a;
+        if (<int>(a) > 47 && <int>(a) < 123) // 0~z 까지만 입력받도록
+            break;
+        continue;
+
+    }
+
+    cout << <int>(a);
+
+    return 0;
+}

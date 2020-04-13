2cf78e823cd72f1df475f8a634e214fabc541144
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-13 09:09:45 +0900

    Create BOJ 2675.cpp

---
+++ BOJ/BOJ 2675.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+//백준 2675
+
+int main(void)
+{
+    string P;
+    int T = 0, R = 0;
+
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> R;
+        cin >> P;
+        int size = P.size();
+
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = 0; k < R; k++)
+            {
+                cout << P[j];
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}

d8a27b4ec987b38c8966c424db0398aa23add511
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-15 10:26:43 +0900

    Create BOJ 2908.cpp

---
+++ BOJ/BOJ 2908.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//백준 2908
+
+int main(void)
+{
+    string A,B;
+
+    cin >> A >> B;
+
+    reverse(A.begin(), A.end());
+    reverse(B.begin(), B.end());
+
+    int AA = atoi(A.c_str());
+    int BB = atoi(B.c_str());
+
+    if (AA > BB)
+        cout << AA;
+    else
+        cout << BB;
+
+    return 0;
+}

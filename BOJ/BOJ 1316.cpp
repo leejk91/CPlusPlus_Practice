1c1b2d652e36bb9ac27e3ea17683c52d50219f1f
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-16 08:35:19 +0900

    Create BOJ 1316.cpp

---
+++ BOJ/BOJ 1316.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+//백준 1316
+void setting()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+int main(void)
+{
+    setting();
+
+    string A;
+    int T = 0;
+
+    cin >> T;
+
+    int sum = T;
+
+    for (int a = 0; a < T; a++)
+    {
+        cin >> A;
+
+        bool check[27];
+
+        for (int i = 0; i <= 26; i++)
+        {
+            check[i] = false;
+        }
+
+        for (int i = 0; i < A.size(); i++)
+        {
+            int W = static_cast<int>(A[i])-97;
+            
+            if (check[W] == true)
+            {
+                sum--;
+                break;
+            }
+            else if (check[W] == false)
+            {
+                if (A[i] != A[i + 1])
+                {
+                    check[W] = true;
+                }
+            }
+
+            
+        }
+
+
+    }
+
+    cout << sum;
+    return 0;
+}

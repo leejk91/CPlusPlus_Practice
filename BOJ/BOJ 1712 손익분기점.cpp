ac3c99e873e7d63ca97d8f30b6a8a494b23ba78a
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-16 13:09:34 +0900

    Create BOJ 1712 손익분기점.cpp

---
+++ BOJ/BOJ 1712 손익분기점.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//백준 1712 손익분기점
+
+
+void setting()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+int main(void)
+{
+    setting();
+
+    int A = 0, B = 0, C = 0;
+
+    cin >> A >> B >> C;
+
+    int i = 0;
+
+    if (C <= B)
+    {
+        cout << -1;
+    }
+    else if (C > B)
+    {
+        cout<<(A / (C - B)) + 1;
+    }
+
+   
+
+    return 0;
+}

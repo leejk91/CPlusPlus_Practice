c4e9e5f83755aa77171627815228f30cf6e44e26
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-17 10:26:28 +0900

    Create BOJ 10870.cpp

---
+++ BOJ/BOJ 10870.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+//백준 10870: 피보나치 수 5
+
+int sum[21] = { 0,1, }; // 피보나치 수는 0, 1부터 시작
+int N = 0; // 찾는 위치를 저장
+
+void fibo(int n) 
+{
+    if ((N-n) == N )
+    {
+        return;
+    }
+    else
+    {
+        sum[(N - n)+2] = sum[N - n] + sum[(N - n)+1];
+        fibo(n - 1);
+    }
+}
+
+
+
+void setting() // 입출력 속도 가속
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+}
+
+int main(void)
+{
+    setting();
+
+    
+
+    cin >> N;
+
+    fibo(N);
+
+    cout << sum[N];
+}

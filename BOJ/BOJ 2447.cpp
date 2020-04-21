0eeecf3a6ea540fadd51d2d4b76bd6bdba542a14
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-21 12:55:30 +0900

    Create BOJ 2447.cpp

---
+++ BOJ/BOJ 2447.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+//백준 2447 : 별 찍기 - 10
+
+void setting() // 입출력 속도 가속
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+}
+
+void star(int i, int j, int N)
+{
+    if ((i / N) % 3 == 1 && (j / N) % 3 == 1) {
+        cout << ' ';
+    }
+    else
+    {
+        if (N / 3 == 0)
+            cout << '*';
+        else
+            star(i, j, N / 3);
+    }
+}
+
+
+int main() {
+
+    setting();
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            star(i, j, N);
+
+        cout << '\n';
+
+    }
+}

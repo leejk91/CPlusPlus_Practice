589c48316e11e0a6d4c567300fc41b9023d6b382
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-24 19:01:54 +0900

    Update BOJ 2751.cpp

--- BOJ/BOJ 2751.cpp
+++ BOJ/BOJ 2751.cpp
@@ -20,25 +20,26 @@ int main() {
 
     int N = 0;
 
-    cin >> N;
+    cin >> N; // 수의 갯수 입력
 
     int* K = new int[N];
 
     for (int i = 0; i < N; i++)
     {
-        cin >> K[i];
+        cin >> K[i]; // 입력
     }
 
 
 
-    sort(K, K + N);
+    sort(K, K + N); // STL에서 제공하는 알고리즘 중에 sort
+    // sort(arr,arr+N)
 
 
 
 
     for (int i = 0; i < N; i++)
     {
-        cout << K[i] << "\n";
+        cout << K[i] << "\n"; // 출력
     }
 
 }

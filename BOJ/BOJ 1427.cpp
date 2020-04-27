e76336edbcbd514adb70b4d7f21b9cd0fab49832
leejk91 <63459361+leejk91@users.noreply.github.com>
2020-04-27 18:15:39 +0900

    Create BOJ 1427.cpp

---
+++ BOJ/BOJ 1427.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+//백준 1427 소트 인사이드
+
+
+int main(void) 
+{
+
+	char number[100];
+
+	cin >> number;
+
+	sort(number, number + strlen(number)); // 정렬
+
+	for (int i = strlen(number)-1; i >=0 ; i--) // 내림차순으로 출력
+	{
+		cout << number[i];
+	}
+
+}

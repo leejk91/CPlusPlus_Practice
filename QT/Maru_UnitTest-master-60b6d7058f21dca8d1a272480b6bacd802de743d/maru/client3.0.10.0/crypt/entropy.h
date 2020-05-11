5d67ec35068f63bb19bc8d5f9e8e23283777dfe9
JeongKi Lee <63459361+leejk91@users.noreply.github.com>
2020-05-11 17:49:56 +0900

    ㅁ

--- QT/Maru_UnitTest-master-60b6d7058f21dca8d1a272480b6bacd802de743d/maru/client3.0.10.0/crypt/entropy.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-#include "hmac_drbg.h"
-#ifndef DUKPT_SERVER
-#include "dukpt_cli_config.h"
-#endif
-#define DAMO_CRYPT_SUCCESS                        00000 /* Success */
-
-int DAMO_CRYPT_Get_SHA256_Entropy_Data(void *data, unsigned char *output, size_t output_len);
